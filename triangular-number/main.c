74e852cab125fb576fbbc6f72a5694820e3db97e
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2016-09-05 15:55:04 +0300

    Более быстрый алгоритм вычислений

--- triangular-number/main.c
+++ triangular-number/main.c
@@ -16,22 +16,25 @@
  */
 
 #include <limits.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+int CORRECT_INPUT_MAX_LENGTH = 19; // number of digits in 9223372036854775807
+
 uint64_t InputNumber() {
-  char input[22];
+  char input[CORRECT_INPUT_MAX_LENGTH + 2]; // one more digit for sign and one - for '\0'
   uint64_t number = 0l;
 
   printf("Please, enter your number\n");
-  int err = scanf("%20s", input);
+  int err = scanf("%20s", input); // read CORRECT_INPUT_MAX_LENGTH+1 characters
   if (err == -1){
       printf("Error!");
       return err;
   }
-  printf("%lld\n%s: %li\n", LONG_LONG_MAX, input, strlen(input));
+  printf("%lld\n%s: %li\n", INT64_MAX, input, strlen(input));
   if ((input[0] == '+' && strlen(input) > 20) || (input[0] != '+' && strlen(input) > 19)) {
       printf("String too long!");
       return 0;
@@ -49,7 +52,7 @@ uint64_t InputNumber() {
 
   err = sscanf(input, "%llu", &number);
 
-  if (number > LONG_LONG_MAX) {
+  if (number > INT64_MAX) {
     printf("Your number is too high!\n");
     return 0;
   }
@@ -63,28 +66,19 @@ uint64_t InputNumber() {
   return number;
 }
 
-long ComputeTriangularNumber(int64_t number) {
-  long iterator = 0l;
-  while (number > 0l) {
-    number = number - (iterator + 1);
-    iterator++;
-  }
-  if (number == 0l) return iterator;
-  else return 0l;
+int32_t ComputeTriangularNumber(int64_t number) {
+  double number_f = (double)number;
+  double result_f = 0.5*(sqrt(1. + 8.*number_f) - 1.);
+  int64_t result = (int64_t)result_f;
+  if (result*(result+1)/2 == number) return result;
+  else return 0;
 }
 
 int main()
 {
   uint64_t number = InputNumber();
 
-  int err = 0;
-  if (err != -1) {
-    printf("It is # %ld in the sequence of triangular numbers.", ComputeTriangularNumber((int64_t)number));
-  }
-  else {
-    printf("error! code: %d\n", err);
-    return 0;
-  }
+  printf("It is # %ld in the sequence of triangular numbers.", ComputeTriangularNumber((int64_t)number));
 
   return 0;
 }
